0b09a43c97c9a59bbdd816c582a66cdc215f3f3c
Furina-Fontaine08 <tatsukihirage08@gmail.com>
2024-12-11 03:37:29 +0800

    Update??

--- ProjectPM/jaildata.sql
+++ ProjectPM/jaildata.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 02:53 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 10, 2024 at 08:35 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Database: `jaildata`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appeal`
+--
+
+CREATE TABLE `appeal` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `appeal_type` varchar(255) NOT NULL,
+  `inmate_name` varchar(255) NOT NULL,
+  `appeal_description` varchar(255) NOT NULL,
+  `supporting_docs` varchar(255) NOT NULL,
+  `previous_comms` varchar(255) NOT NULL,
+  `consent` tinyint(1) NOT NULL,
+  `terms` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medical`
+--
+
+CREATE TABLE `medical` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `home_address` varchar(255) NOT NULL,
+  `inmate_name` varchar(255) NOT NULL,
+  `inmate_age` int(11) NOT NULL,
+  `medical_condition` varchar(255) NOT NULL,
+  `medical_history` varchar(255) NOT NULL,
+  `assistance` varchar(255) NOT NULL,
+  `additional_info` text NOT NULL,
+  `terms` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,9 +88,62 @@ CREATE TABLE `registration` (
 INSERT INTO `registration` (`RegistrationID`, `FullName`, `Email`, `Address`, `DetaineesFullName`, `Relationship`, `IDPicture`, `RegistrationStatus`, `Password`) VALUES
 (10, 'asdasd', 'sada@gmail.com', 'asdasd', 'asdasd', 'parent', '6758476d6d3e3.jpg', 'Pending', '25634201');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `virtual`
+--
+
+CREATE TABLE `virtual` (
+  `id` int(11) NOT NULL,
+  `visitor_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `id_type` varchar(255) NOT NULL,
+  `id_number` varchar(255) NOT NULL,
+  `inmate_name` varchar(255) NOT NULL,
+  `inmate_id` varchar(255) NOT NULL,
+  `number_call` varchar(255) NOT NULL,
+  `relationship` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL,
+  `terms` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitation`
+--
+
+CREATE TABLE `visitation` (
+  `id` int(11) NOT NULL,
+  `visitor_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `id_type` varchar(255) NOT NULL,
+  `id_number` varchar(255) NOT NULL,
+  `inmate_name` varchar(255) NOT NULL,
+  `inmate_id` varchar(255) NOT NULL,
+  `relationship` varchar(255) NOT NULL,
+  `visit_reason` text NOT NULL,
+  `terms_accepted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appeal`
+--
+ALTER TABLE `appeal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `medical`
+--
+ALTER TABLE `medical`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `registration`
@@ -58,14 +153,50 @@ ALTER TABLE `registration`
   ADD UNIQUE KEY `Email` (`Email`);
 
 --
+-- Indexes for table `virtual`
+--
+ALTER TABLE `virtual`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitation`
+--
+ALTER TABLE `visitation`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `appeal`
+--
+ALTER TABLE `appeal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `medical`
+--
+ALTER TABLE `medical`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
   MODIFY `RegistrationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `virtual`
+--
+ALTER TABLE `virtual`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `visitation`
+--
+ALTER TABLE `visitation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
